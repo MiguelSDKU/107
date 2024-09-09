d879ace44ccaad6601282ff9ba566ca3156aa414
MikeCota20 <miguel.cotagallegos@cesunbc.edu.mx>
2024-09-08 22:40:22 -0700

    assignments

--- server/server.py
+++ server/server.py
@@ -1,8 +1,11 @@
-from flask import Flask 
+from flask import Flask, request
 import json
+from config import db
 
 
 app = Flask(__name__)
+
+products=[]
 
 @app.get("/")
 def home():
@@ -18,5 +21,27 @@ def about():
 def footer():
   pageName={"pageName":"organika"}
   return json.dumps(pageName)
+
+@app.get("/api/products")
+def read_products():
+  return json.dumps(products) 
+
+@app.post("/api/products")
+def saveProducts():
+  item = request.get_json()
+  # products.append(item )
+  db.products.insert_one(item)
+  print(item)
+  return json.dumps(item)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+  update_item= request.get_json()
+  if 0<=index<len(products):
+    products[index]=update_item
+    return json.dumps(update_item)
+  else:
+    return "That index does not exist"
+  
 
 app.run(debug=True)
